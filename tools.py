3f4e629e1550a8273aa258b5bfcc4fd26a90d862
hyfh <zhangleihebut@outlook.com>
2018-06-22 21:16:28 +0800

    tool to fetch content from novel site

--- tools.py
+++ tools.py
@@ -1,32 +1,44 @@
 #!python3
 # -*- coding: utf-8 -*-
 
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 
-def read_str_from_url(url):
-    content = urlopen(url).read()
-    
-    soup = BeautifulSoup(content, "html.parser")
+black_list = ('加入书架', '小说网', '加入书签', '投票推荐', '上一页', '下一页', '错误举报')
+
+def html2text(html):
+    for script in html(["script", "style"]):
+        script.extract()
+    for br in html.find_all('br'):
+        br.replace_with('\n' + br.text)
+    text = html.get_text()
+    lines = (line.strip() for line in text.splitlines())
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # truncate with blacklist
+    def test(line):
+        if len(line) > 30:
+            return True
+        for k in black_list:
+            if k in line:
+                return False
+        return True
+    chunks = filter(test, chunks)
+    return '\n\n'.join(chunk for chunk in chunks if chunk)
+
+def read_content_from_url(url):
+    content = requests.get(url, timeout=1.5)
+
+    soup = BeautifulSoup(content.content, 'html.parser')
     body = soup.find('body')
     if body != None:
         soup = body
 
-    s = soup.get_text('\n', True)
-    s = s.replace('\n\r', '\n')
-    s = s.replace('\r\n', '\n')
-    for i in [5, 5, 4, 4, 4, 3, 3, 3, 3]:
-        s = s.replace('\n'*i, '\n\n')
-
-    s = s.replace('&nbsp;',' ')
+    s = html2text(soup)
     return s
 
-
-def main():
-    s = read_str_from_url('http://xs.dmzj.com/1122/3990/24478.shtml')
-    with open('c.txt', 'w', encoding='utf-8') as f:
-        f.write(s)
-
-
 if __name__ == '__main__':
-    main()
+    u = "http://www.7kankan.com/files/article/html/42/42515/7603088.html"
+    s = read_content_from_url(u)
+    print(s)
+    with open('c.txt', 'w', encoding='utf8') as f:
+        f.write(s)
